35102af34b06c9337496a58bf998bd134bee0236
naigutstein <naomigutstein2017@u.northwestern.edu>
2017-04-03 18:18:13 -0500

    starting and ending date updated to calendar selector

--- YellowTeamProject/src/pages/main-search/main-search.ts
+++ YellowTeamProject/src/pages/main-search/main-search.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { ListPage } from '../list/list';
 import { NavController } from 'ionic-angular';
-
+import { Events } from 'ionic-angular';
 
 @Component({
   selector: 'page-main-search',
   templateUrl: 'main-search.html'
 })
 export class MainSearchPage {
-  constructor(public nav: NavController) { }
+  constructor(public nav: NavController, public event: Events) { }
   search(){
+  	this.event.publish('search')
   	this.nav.push(ListPage);
   }
+
 }
